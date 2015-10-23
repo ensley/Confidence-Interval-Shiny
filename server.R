15bbee41c484ec1e21c5af767e09ac127daaaf9d
ensley92 <johnensley17@gmail.com>
2015-10-22 23:53:56 -0400

    made it cooler
    so much cooler

--- server.R
+++ server.R
@@ -1,45 +1,85 @@
 library(shiny)
-library(ggplot2)
+library(ggvis)
+library(dplyr)
 
 shinyServer(function(input, output, session) {
   
+  # alpha level for confidence intervals
   inputAlpha <- reactive({
     as.numeric(input$alpha)
   })
   
+  # counter for number of intervals that don't contain the mean
+  count <- reactiveValues(c=0)
+  
   create_data <- reactive({
+
     means <- rep(NA, length=input$N)
     sds <- rep(NA, length=input$N)
     for(i in 1:input$N) {
-      data <- rnorm(as.numeric(input$size), input$mean, input$sd)
-      means[i] <- mean(data)
-      sds[i] <- sd(data)
+      d <- rnorm(as.numeric(input$size), input$mean, input$sd)
+      means[i] <- mean(d)
+      sds[i] <- sd(d)
     }
-    df <- data.frame(means=means, sds=sds)
-    df
+    data.frame(means=means, sds=sds)
+  
   })
   
-  output$plot <- renderPlot({
-    data <- create_data()
+  create_intervals <- reactive({
     
+    data <- isolate({ create_data() })
+
     x <- 1:input$N
     lower <- rep(NA, length=input$N)
     upper <- rep(NA, length=input$N)
     notcontained <- rep(NA, length=input$N)
     
     for(i in x) {
-      se <- qt(1-(1-inputAlpha())/2, df=as.numeric(input$size)-1)*data$sds[i]/sqrt(as.numeric(input$size))
+      se <- qt(1-(1-input$alpha)/2, df=as.numeric(input$size)-1)*data$sds[i]/sqrt(as.numeric(input$size))
       lower[i] <- data$means[i]-se
       upper[i] <- data$means[i]+se
       notcontained[i] <- !(lower[i] < input$mean & upper[i] > input$mean)
     }
     
     df <- data.frame(x=x, mean=data$means, l=lower, u=upper, c=notcontained)
-    ggplot(df, aes(x=x, y=mean)) +
-      geom_point(size=4, aes(color=factor(c))) +
-      geom_errorbar(aes(ymax=u, ymin=l, color=factor(c))) +
-      scale_color_manual(values=c('black','blue')) +
-      geom_hline(yintercept=input$mean)
+    
+    isolate({
+      count$c <- sum(notcontained)
+    })
+    
+    df
     
   })
+  
+  data_tooltip <- function(p) {
+    if (is.null(p)) return(NULL)
+    
+    all_pts <- isolate(create_intervals())
+    pt <- all_pts[all_pts$x == p$x, ]
+    
+    contained_string = " "
+    if(pt$c) { contained_string = " not " }
+    
+    paste0("<b>Sample mean: </b>", round(pt$mean,3), "<br>",
+           "<b>Interval: </b>(", round(pt$l,3), ", ", round(pt$u,3), ")<br>",
+           "True mean is", contained_string, "contained in the interval.")
+  }
+  
+  vis <- reactive({
+    
+    create_intervals %>%
+      ggvis(x = ~mean, y = ~x) %>%
+      layer_rects(~l, ~x-as.numeric(input$N)/2000, x2 = ~u, y2 = ~x+as.numeric(input$N)/2000, stroke = ~as.factor(c)) %>%
+      layer_points(size.hover := 200, fillOpacity := 0.8, fillOpacity.hover := 0.6) %>%
+      add_tooltip(data_tooltip, 'hover') %>%
+      set_options(height = 550, keep_aspect = T)
+    
+  })
+  
+  vis %>% bind_shiny('plot')
+  
+  output$count <- renderText({
+    paste0(round(as.numeric(count$c)/as.numeric(input$N)*100,2),'% (',count$c,' out of ', input$N, ') confidence intervals do not include the true mean. \nAt a confidence level of ', inputAlpha(), ', we would expect this to be about ', round((1-inputAlpha())*100), '%.')
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,24 +1,26 @@
 library(shiny)
+library(ggvis)
 
 shinyUI(fluidPage(
-  title='Confidence Intervals on Means',
+  titlePanel('Confidence Intervals on Means'),
   
-  plotOutput('plot'),
-  
-  fluidRow(
-    column(4,
+  sidebarLayout(
+    sidebarPanel(
       selectizeInput('N', 'Number of samples',
                      choices=c('10','20','50','100','500'), selected=100,
                      options=list(create=T)),
       selectizeInput('size', 'Sample size',
                      choices=c('5','10','25','50','100'), selected=25,
                      options=list(create=T)),
-      sliderInput('alpha', 'Confidence level', min=0, max=1, step=0.01, value=0.95)
-    ),
-    
-    column(4,
+      sliderInput('alpha', 'Confidence level', min=0, max=1, step=0.01, value=0.95),
       numericInput('mean', 'Mean', value=0),
       numericInput('sd', 'Standard deviation', value=1)
+    ),
+    
+    mainPanel(
+      ggvisOutput('plot'),
+      textOutput('count'),
+      verbatimTextOutput('hover_info')
     )
   )
 ))
